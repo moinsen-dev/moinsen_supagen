da029338235d248fe14bd49f55725451f24539e1
Ulrich Diedrichsen <uli@moinsen.dev>
2023-10-20 17:39:50 +0200

    Fix realtime

--- lib/moinsen_supagen.dart
+++ lib/moinsen_supagen.dart
@@ -1,8 +1,11 @@
-// ignore_for_file: unnecessary_library_directive
+// ignore_for_file: unnecessary_library_directive, depend_on_referenced_packages
 library moinsen_supagen;
 
 export 'package:equatable/equatable.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:realtime_client/realtime_client.dart';
+export 'package:supabase/src/supabase_stream_builder.dart'
+    show SupabaseStreamBuilder;
 export 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
 
 export 'utils/_index.dart';
